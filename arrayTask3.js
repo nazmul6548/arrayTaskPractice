1bf7735e5af4e4aa5a7c14e4dbdb32193597f6cc
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-02-01 11:38:53 +0600

    task

---
+++ arrayTask3.js
@@ -0,0 +1,22 @@
+// Task 3
+// Use a for...of loop to concatenate all the elements of an array into a single string.
+
+// Input: var numbers = ['Tom', 'Tim', 'Tin', 'Tik']
+
+// Output:
+
+// 'TomTimTinTik'
+// ans:
+
+var numbers = ['Tom', 'Tim', 'Tin', 'Tik']
+var ans =""
+
+for (const after of numbers) {
+    ans = ans + after
+
+    // console.log(after);
+    
+    
+}
+console.log(typeof ans);
+console.log(ans);
