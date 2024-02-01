480ff7f5209e62f7eee2a8b9474253d2fa8e0c63
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-02-01 11:25:28 +0600

    task

--- arrayTask2.js
+++ arrayTask2.js
@@ -8,15 +8,36 @@
 // [12, 98, 78, 46]
 
 const numbers = [12, 98, 5, 41, 23, 78, 46];
-let myStore  =[]
-for (let i = 0; i < numbers.length; i++) {
-    const x = numbers[i];
-    if (x % 2 === 0) {
-        myStore.push(x);
-        console.log(x);
+// let myStore  =[]
+// for (let i = 0; i < numbers.length; i++) {
+//     const x = numbers[i];
+//     if (x % 2 === 0) {
+//         myStore.push(x);
+//         console.log(x);
          
+//     }
+    
+    
+// }
+// console.log(myStore);
+
+
+
+
+// for while
+
+let store = [];
+let me = 0;
+
+while (me < numbers.length) {
+    const myNu = numbers[me];
+    // console.log(myNu);
+    if ( myNu % 2 === 0) {
+        store.push(myNu)
+        
     }
-    
-    
+    me++;
 }
-console.log(myStore);
+console.log(store);
+ 
+
